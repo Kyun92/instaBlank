a2dff59a1a8513ffb14230c89b5b22b4c1fdef3e
kyun <lsk9105@gmail.com>
2019-03-26 15:46:44 +0900

    Updates

---
+++ precache-manifest.f271d1db902b2bf18711502be5dd4332.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6fb1a402ee717e045e8",
+    "url": "/Blank/static/js/runtime~main.0ba0a3e9.js"
+  },
+  {
+    "revision": "4ba228ef2c4aff698398",
+    "url": "/Blank/static/js/main.80873b72.chunk.js"
+  },
+  {
+    "revision": "35a1086de74d37a61382",
+    "url": "/Blank/static/js/2.fd6132b8.chunk.js"
+  },
+  {
+    "revision": "4ba228ef2c4aff698398",
+    "url": "/Blank/static/css/main.f7630ba3.chunk.css"
+  },
+  {
+    "revision": "049c05a1e8b72a03d64a5627f0d7d7b2",
+    "url": "/Blank/index.html"
+  }
+];
